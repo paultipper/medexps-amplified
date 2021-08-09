53cd616caf0663485b88b8df59d30ad58a97d21e
Paul Tipper <ptipper@gmail.com>
2021-08-09 09:54:21 +0100

    Defined the API and hosting configuration and deployed to AWS using the Amplify CLI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+/* src/App.js */
+import React, { useEffect, useState } from 'react'
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import { createBlog } from './graphql/mutations'
+import { listBlogs } from './graphql/queries'
+import { withAuthenticator } from '@aws-amplify/ui-react'
 
-function App() {
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+const initialState = { name: '' }
+
+const App = () => {
+  const [formState, setFormState] = useState(initialState)
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    fetchBlogs()
+  }, [])
+
+  function setInput(key, value) {
+    setFormState({ ...formState, [key]: value })
+  }
+
+  async function fetchBlogs() {
+    try {
+      const blogData = await API.graphql(graphqlOperation(listBlogs))
+      const blogs = blogData.data.listBlogs.items
+      setBlogs(blogs)
+    } catch (err) { console.log('error fetching blogs') }
+  }
+
+  async function addBlog() {
+    try {
+      if (!formState.name ) return
+      const blog = { ...formState }
+      setBlogs([...blogs, blog])
+      setFormState(initialState)
+      await API.graphql(graphqlOperation(createBlog, {input: blog}))
+    } catch (err) {
+      console.log('error creating blog:', err)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <h2>Amplify Blogs</h2>
+      <input
+        onChange={event => setInput('name', event.target.value)}
+        style={styles.input}
+        value={formState.name}
+        placeholder="Name"
+      />
+      <button style={styles.button} onClick={addBlog}>Create Blog</button>
+      {
+        blogs.map((blog, index) => (
+          <div key={blog.id ? blog.id : index} style={styles.blog}>
+            <p style={styles.blogName}>{blog.name}</p>
+          </div>
+        ))
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+const styles = {
+  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
+  blog: {  marginBottom: 15 },
+  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
+  blogName: { fontSize: 20, fontWeight: 'bold' },
+  blogDescription: { marginBottom: 0 },
+  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
+}
+
+export default withAuthenticator(App)
